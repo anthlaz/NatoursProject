27362129059e56ae8847d026525e967cb73813df
lazosa <lazosa@deakin.edu.au>
2022-01-27 09:11:45 +1100

    uploaded sync/async file system code

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,10 +1,21 @@
-// read and write files
+// read and write files - blocking and unblocking method
 const fs = require('fs');
 
-const inputText = fs.readFileSync('./txt/input.txt', 'utf-8');
+// Blocking, synchronously
+// const inputText = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(inputText);
+// const textOut = `This is what we know about the avocado: ${inputText}.\n Created on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File has been written..');
 
-console.log(inputText);
+// Non-blocking, asynchronously
+fs.readFile('./txt/start.txt', 'utf-8',(err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8',(err, data2) => {
+        console.log(data2);
 
-const textOut = `This is what we know about the avocado: ${inputText}.\n Created on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File has been written..');
+        fs.writeFile('./txt/final.txt', `${data1}\n${data2}`, 'utf-8', err => {
+            console.log('File has been written to final.txt');
+        })
+    });
+});
+console.log('This will execute as file is being read');
