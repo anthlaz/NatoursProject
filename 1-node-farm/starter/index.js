278b03ce06b1fff35b8b32962dcf3b991b1e6db4
lazosa <lazosa@deakin.edu.au>
2022-02-11 16:42:09 +1100

    upgraded to use prettier and npm

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,7 +1,13 @@
-// read and write files - blocking and unblocking method
+// using core modules
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+// using third part modules
+const slugify = require('slugify');
+
+// using my modules
+const replaceTemplate = require('../modules/replaceTemplate');
 
 ////////////// FILES //////////////
 
@@ -26,9 +32,6 @@ const url = require('url');
 // });
 // console.log('This will execute as file is being read');
 
-
-
-
 ////////////// SERVERS //////////////
 
 // create the server
@@ -42,83 +45,85 @@ const url = require('url');
 //     console.log('Listening to requests on port 8000');
 // })
 
-
 ////////////// READING DATA/FILES ONCE //////////////
 
-// functions
-const replaceTemplate = (template, product) => {
-    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
+// // functions
+// const replaceTemplate = (template, product) => {
+//     let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+//     output = output.replace(/{%IMAGE%}/g, product.image);
+//     output = output.replace(/{%PRICE%}/g, product.price);
+//     output = output.replace(/{%FROM%}/g, product.from);
+//     output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+//     output = output.replace(/{%QUANTITY%}/g, product.quantity);
+//     output = output.replace(/{%DESCRIPTION%}/g, product.description);
+//     output = output.replace(/{%ID%}/g, product.id);
 
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+//     if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
 
-    return output;
-}
+//     return output;
+// }
 
 // html
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 // data
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const productData = JSON.parse(data);
+
+// slugify
+
+const slugs = productData.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 ////////////// ROUTING //////////////
 
 // how do we give a different response depedent on the url path? We can use if/else
 
 const server = http.createServer((req, res) => {
-    
-    const {query, pathname} = url.parse(req.url, true);
+  const { query, pathname } = url.parse(req.url, true);
 
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    // step 1: load the overview html page - done above
+    // step 2: change response header content type to html
+    // step 3: iterate through productData so we can send that information back
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-    // Overview page
-    if (pathname === '/' || pathname === '/overview') {
-        // step 1: load the overview html page - done above
-        // step 2: change response header content type to html 
-        // step 3: iterate through productData so we can send that information back
-        res.writeHead(200, {'Content-type': 'text/html'});
-
-        // we don't want an array but one big string
-        const cardsHtml = productData.map(el => replaceTemplate(tempCard, el)).join('');
-        // replace the placeholder in the template overview html page
-        const output = tempOverview.replace('{%PRODUCT_CARD%}', cardsHtml);
-        // console.log(cardsHtml);
-        res.end(output);
+    // we don't want an array but one big string
+    const cardsHtml = productData.map((el) => replaceTemplate(tempCard, el)).join('');
+    // replace the placeholder in the template overview html page
+    const output = tempOverview.replace('{%PRODUCT_CARD%}', cardsHtml);
+    // console.log(cardsHtml);
+    res.end(output);
 
     // product page
-    } else if (pathname === '/product') {
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
 
-        res.writeHead(200, {'Content-type': 'text/html'});
-
-        // which product do we want to display, we grab it from the productData object
-        const product = productData[query.id];     
-        const output = replaceTemplate(tempProduct, product);
-        res.end(output);
+    // which product do we want to display, we grab it from the productData object
+    const product = productData[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     // API page
-    } else if (pathname === '/api') {
-        res.writeHead(200, {
-        'Content-type': 'application/json'});
-        res.end(productData);
+  } else if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    res.end(productData);
 
     // Not found page
-    } else {
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'My-own-header':'Just some bullshit I made up',
-        });
-        res.end('<h1>Page not found!</h1>');
-    }
-})
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'My-own-header': 'Just some bullshit I made up',
+    });
+    res.end('<h1>Page not found!</h1>');
+  }
+});
 
 // now we listen on the server
 server.listen(8000, '127.0.0.1', () => {
-    console.log('Listening to requests on port 8000');
-})
+  console.log('Listening to requests on port 8000');
+});
